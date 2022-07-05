77a7a7f01a7e3f6185f7a2f1a9e70c845f6824c7
Zach Stearns <zach.stearns92@gmail.com>
2022-07-05 11:27:44 -0600

    Move baseUrl, pullRequestState, and resultsPerPage to constants file

---
+++ src/constants.js
@@ -0,0 +1,7 @@
+const constants = {
+  baseUrl: 'https://api.github.com',
+  pullRequestState: 'open',
+  resultsPerPage: 100,
+};
+
+export default constants;
--- src/services/githubService.js
+++ src/services/githubService.js
@@ -1,11 +1,18 @@
 import axios from 'axios';
+import constants from '../constants.js';
+
+const {
+  baseUrl,
+  pullRequestState,
+  resultsPerPage,
+} = constants;
 
 const getOpenPullRequests = async ({
   organization,
   repository,
   pageNumber,
 }) => {
-  return axios.get(`https://api.github.com/repos/${organization}/${repository}/pulls?state=open&per_page=100&page=${pageNumber}`, {
+  return axios.get(`${baseUrl}/repos/${organization}/${repository}/pulls?state=${pullRequestState}&per_page=${resultsPerPage}&page=${pageNumber}`, {
     auth: { username: process.env.GITHUB_USERNAME, token: process.env.GITHUB_ACCESS_TOKEN }
   });
 };
@@ -15,7 +22,7 @@ const getPullRequest = async ({
   repository,
   pullRequestNumber,
 }) => {
-  return axios.get(`https://api.github.com/repos/${organization}/${repository}/pulls/${pullRequestNumber}`, {
+  return axios.get(`${baseUrl}/repos/${organization}/${repository}/pulls/${pullRequestNumber}`, {
     auth: { username: process.env.GITHUB_USERNAME, token: process.env.GITHUB_ACCESS_TOKEN }
   });
 }
