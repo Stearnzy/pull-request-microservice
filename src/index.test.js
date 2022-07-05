bb226a3bc96c1e56f9747b9a4e7fc66fb135a0a6
Zach Stearns <zach.stearns92@gmail.com>
2022-07-05 14:13:26 -0600

    Write test for nonexistent endpoint

--- src/index.test.js
+++ src/index.test.js
@@ -10,3 +10,11 @@ it('calls the root endpoint successfully', async () => {
   expect(res.status).toBeLessThan(300);
   expect(res.text).toBe('Welcome to the GitHub pull request microservice!')
 });
+
+it('will return an error if the endpoint does not exist', async () => {
+  const res = await request.get('/api/turkey');
+
+  expect(res.status).toBeGreaterThanOrEqual(400);
+  expect(res.status).toBeLessThan(500);
+  expect(res.body.error).toBe('Route not found');
+})
